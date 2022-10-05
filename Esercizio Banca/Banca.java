dcfeb17cf03af75e49b15befee84dcd0ae6597a5
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2022-10-05 17:23:33 +0200

    Add files via upload

---
+++ Esercizio Banca/Banca.java
@@ -0,0 +1,11 @@
+public class Banca {
+    ContoCorrente[] conti = new ContoCorrente[6];
+
+
+    conti[1] = new ContoCorrente("Mario Rossi", "IT123456789012345678901234", 1000);
+    conti[2] = new ContoCorrente("Luigi Bianchi", "IT123456789012345678901235", 2000);
+    conti[3] = new ContoCorrente("Giovanni Verdi", "IT123456789012345678901236", 3000);
+    conti[4] = new ContoCorrente("Giuseppe Neri", "IT123456789012345678901237", 4000);
+    conti[5] = new ContoCorrente("Antonio Gialli", "IT123456789012345678901238", 5000);
+    
+}
---
+++ Esercizio Banca/ContoCorrente.java
@@ -0,0 +1,28 @@
+public class ContoCorrente {
+    public double saldo;
+    public String intestatario, iban;
+
+    public ContoCorrente(String intestatario, String iban, double saldo) {
+        this.intestatario = intestatario;
+        this.iban = iban;
+        this.saldo = saldo;
+    }
+
+    public void preleva(double importo) {
+        if (importo > saldo) {
+            System.out.println("Saldo insufficiente");
+        } else {
+            saldo -= importo;
+        }
+    }
+
+    public void deposita(double importo) {
+        saldo += importo;
+    }
+
+    public void stampa() {
+        System.out.println("Intestatario: " + intestatario);
+        System.out.println("IBAN: " + iban);
+        System.out.println("Saldo: " + saldo);
+    }
+}
