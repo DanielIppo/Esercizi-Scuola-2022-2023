fd57a8ea3c7b90f7661f23f8f2431693dd534fa4
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2022-10-06 14:53:56 +0200

    Update and rename Main.java to MainCC.java

---
+++ Esercizio Banca/MainCC.java
@@ -0,0 +1,7 @@
+public class MainCC {
+    public static void main(String[] args) {
+        Banca banca = new Banca();
+        banca.saveSVG();
+    }
+}
+//https://www.codejava.net/java-se/file-io/how-to-read-and-write-text-file-in-java
