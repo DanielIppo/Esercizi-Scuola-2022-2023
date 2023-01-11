00643c235476063f4616399d1c1d2f95e877154c
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2023-01-11 12:05:30 +0100

    Update main.java

--- PausaDidattica/main.java
+++ PausaDidattica/main.java
@@ -1,1 +1,13 @@
-
+public class Main {
+    public static void main(String[] args){
+        Palazzo palazzo1 = new Palazzo(3, 1000, "Via Roma", 1, "Roma");
+        Palazzo palazzo2 = new Palazzo(4, 150, "Via Milano", 2, "Milano");
+        Palazzo palazzo3 = new Palazzo(5, 200, "Via Napoli", 3, "Napoli");
+        palazzo1.stampa();
+        System.out.println();
+        palazzo2.stampa();
+        System.out.println();
+        palazzo3.stampa();
+        System.out.println();
+    }
+}
