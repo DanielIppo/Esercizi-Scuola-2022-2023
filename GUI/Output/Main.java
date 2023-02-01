29906fe88622de071a7bfad51ca0fc8cd1a8b03b
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2023-02-01 10:57:54 +0000

    Giorno 01/02/2023

--- GUI/Output/Main.java
+++ GUI/Output/Main.java
@@ -3,13 +3,13 @@ import javax.swing.*;
 import java.awt.*;
 public class Main extends JFrame {
     public Main() {
-        setTitle("Simple GUI");
+        setTitle("GUI Semplice");
         setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         panel.setBackground(Color.LIGHT_GRAY);
         add(panel);
-        JLabel label = new JLabel("Hello, World!");
+        JLabel label = new JLabel("Ciao Mondo!");
         panel.add(label);
         setVisible(true);
     }
