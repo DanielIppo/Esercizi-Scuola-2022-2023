0c5d5a39d6518ac65b6ef94b68f34246c4c86f38
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2023-03-21 23:16:21 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        Alimentari[] alimentari = new Alimentari[10000];
-        Casalinghi[] casalinghi = new Casalinghi[10000];
-        Abbigliamento[] abbigliamento = new Abbigliamento[10000];
-        new Magazzino(alimentari, casalinghi, abbigliamento);
-    }
-}
